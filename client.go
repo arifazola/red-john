00e6869816d7a0a70cb0e56dd6a97b2510d54f3e
Ari <afazola@gmail.com>
2026-04-15 17:08:56 +0700

    reconnecting

--- client.go
+++ client.go
@@ -17,65 +17,76 @@ type Client struct {
 	inMemoryStore *module.InMemoryStore
 }
 
-func(client *Client) ConnectToLeader(leaderAddr string, context context.Context, numOfRetry int) {
-	retry := numOfRetry
+func(client *Client) ConnectToLeader(leaderAddr string, context context.Context) {
 	maxNumOfRetry := 5
 	
-	conn, err := net.Dial("tcp", leaderAddr)
+	for i := 1; i < maxNumOfRetry; i ++ {
+		conn, err := net.Dial("tcp", leaderAddr)
 
-	if err != nil {
-		if(numOfRetry == maxNumOfRetry){
-			return
-		}
-		fmt.Println("Error connecting to leader server ", err)
-		fmt.Println("Reconnecting ", retry)
-		time.Sleep(2 * time.Second)
-		client.ConnectToLeader(leaderAddr, context, retry + 1)
-		return
-	}
-
-	go func ()  {
-		<-context.Done()
-		conn.Close()
-	}()
-
-	defer conn.Close()
-
-	reader := bufio.NewReader(conn)
-
-	_, writeError := conn.Write([]byte("SYNC_ME\n"))
-
-	if writeError != nil {
-		fmt.Println("error writing message ", writeError)
-		return
-	}
-
-	for {
-		msg, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Println("Lost connection to leader", err)
-			return
+			fmt.Println("Error connecting to leader server ", err)
+
+			select {
+			case <-time.After(2 * time.Second):
+				continue
+			case <-context.Done():
+				fmt.Println("Shutting down signal received")
+				return
+			}
 		}
 
-		fmt.Println("Command from leader", msg)
 
-		var parsedJson map[string]models.Item
-		jsonErr := json.Unmarshal([]byte(msg), &parsedJson)
+		reader := bufio.NewReader(conn)
 
-		//check if parsing the message from leader throws an error
-		//if message cannot be parsed, then it's a command (SET or GET)
-		//if message can be parsed, then it's a syncing data. Follower must store data from leader to it's own inMemoryStore
-		if jsonErr != nil {
-			commands := strings.Fields(msg)
+		_, writeError := conn.Write([]byte("SYNC_ME\n"))
 
-			fmt.Println("Adding command for followers", commands)
-			result, _ := module.CommandRouter(commands, client.inMemoryStore, "")
+		if writeError != nil {
+			fmt.Println("error writing message ", writeError)
+			conn.Close()
+			continue // continue to the next loop to retry 
+		}
 
-			fmt.Println("client command result ", result)
-		} else {
-			fmt.Println("Syncing")
-			client.inMemoryStore.SetAll(parsedJson)
-			fmt.Println("Finished syncing data")
+		for {
+			conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+			msg, err := reader.ReadString('\n')
+			if err != nil {
+				netErr, ok := err.(net.Error)
+				if ok && netErr.Timeout(){
+					select{
+					case <-context.Done():
+						fmt.Println("Shutting down reading")
+						conn.Close()
+						return
+					default:
+						fmt.Println("No message received after 5 second. Timeout")
+						continue
+					}
+				}
+				fmt.Println("Lost connection to leader", err)
+				conn.Close()
+				break //break to get out of this inner loop and retry
+			}
+
+			fmt.Println("Command from leader", msg)
+
+			var parsedJson map[string]models.Item
+			jsonErr := json.Unmarshal([]byte(msg), &parsedJson)
+
+			//check if parsing the message from leader throws an error
+			//if message cannot be parsed, then it's a command (SET or GET)
+			//if message can be parsed, then it's a syncing data. Follower must store data from leader to it's own inMemoryStore
+			if jsonErr != nil {
+				commands := strings.Fields(msg)
+
+				fmt.Println("Adding command for followers", commands)
+				result, _ := module.CommandRouter(commands, client.inMemoryStore, "")
+
+				fmt.Println("client command result ", result)
+			} else {
+				fmt.Println("Syncing")
+				client.inMemoryStore.SetAll(parsedJson)
+				fmt.Println("Finished syncing data")
+			}
 		}
 	}
 
--- main.go
+++ main.go
@@ -40,7 +40,7 @@ func main() {
 		}
 		go func ()  {
 			defer wg.Done()
-			client.ConnectToLeader(*leaderAddr, ctx, 0)
+			client.ConnectToLeader(*leaderAddr, ctx)
 		}()
     }
 
