fe5fb911eb0eaafea5196b15692e4dfa7db1192e
Ari <afazola@gmail.com>
2026-08-20 21:33:52 +0700

    UPDATE INTEGRATED TEST

--- integration_test.go
+++ integration_test.go
@@ -6,7 +6,9 @@ import (
 	"net"
 	"os/exec"
 	"strings"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -53,6 +55,96 @@ func TestSetCommandOneServerActive_Success(t *testing.T){
 	responseMsg := strings.TrimSpace(response)
 
 	assert.Equal(t, "OK", responseMsg, "Message should be OK")
+
+}
+
+func TestSetCommandMultipleServerActive_BroadcastSuccess(t *testing.T){
+	startServer(t, "8080", "")
+
+	startServer(t, "8081", "localhost:8080")
+
+    startServer(t, "8082", "localhost:8080")
+
+    startServer(t, "8082", "localhost:8080")
+    
+
+	conn, err := net.Dial("tcp", "localhost:8080")
+
+	if err != nil {
+		t.Fatal("Error connect to leader", err)
+	}
+
+	defer conn.Close()
+
+	_, err = conn.Write([]byte("SET NAME ARI\n"))
+
+	if err != nil {
+		t.Fatal("Error Making Set Command", err)
+	}
+
+	reader := bufio.NewReader(conn)
+
+	response, err := reader.ReadString('\n')
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	responseMsg := strings.TrimSpace(response)
+
+	assert.Equal(t, "OK", responseMsg, "Message should be OK")
+
+}
+
+func TestSetCommandMultipleServerActive_ShuouldReturnOKWhenOneFollowerIsDeadDuringBroadcast(t *testing.T){
+	startServer(t, "8080", "")
+
+	startServer(t, "8081", "localhost:8080")
+
+    server3 := startServer(t, "8082", "localhost:8080")
+
+    startServer(t, "8083", "localhost:8080")
+    
+	conn, err := net.Dial("tcp", "localhost:8080")
+
+	if err != nil {
+		t.Fatal("Error connect to leader", err)
+	}
+
+	defer conn.Close()
+
+	time.Sleep(5 * time.Second)
+
+	go func ()  {
+		fmt.Println("KILLING PROCESS")
+		server3.Process.Kill()
+	}()
+
+	channel := make(chan string)
+	go func ()  {
+		_, err = conn.Write([]byte("SET NAME ARI\n"))
+
+		if err != nil {
+			fmt.Println("Error Making Set Command", err)
+			channel <- ""
+			return
+		}
+
+		reader := bufio.NewReader(conn)
+
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading line", err)
+			channel <- ""
+			return
+		}
+
+		responseMsg := strings.TrimSpace(response)
+
+		channel <- responseMsg
+	}()
+	
+
+	assert.Equal(t, "OK", <-channel, "Message should be OK")
 
 }
 
@@ -156,6 +248,428 @@ func TestGetCommandOneServerActive_NoData(t *testing.T){
 	assert.Equal(t, "NOT_FOUND", responseMsg, "Message should be NOT_FOUND")
 
 	conn.Close()
+}
+
+func TestConcurentSetCommandOneServerActive_Success(t *testing.T){
+	startServer(t, "8080", "")
+
+	channel := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func ()  {
+		
+		conn, err := net.Dial("tcp", "localhost:8080")
+
+		if err != nil {
+			channel <- 0
+		}
+
+		
+		defer func ()  {
+			wg.Done()
+			conn.Close()
+		}()
+
+		_, err = conn.Write([]byte("SET NAME ARI\n"))
+
+		if err != nil {
+			channel <- 0
+		}
+
+		reader := bufio.NewReader(conn)
+
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			channel <- 0
+		}
+
+		responseMsg := strings.TrimSpace(response)
+
+		if responseMsg == "OK"{
+			channel <- 1
+		} else {
+			channel <- 0
+		}
+	}()
+
+	go func ()  {
+		conn, err := net.Dial("tcp", "localhost:8080")
+
+		if err != nil {
+			channel <- 0
+		}
+
+		
+		defer func ()  {
+			wg.Done()
+			conn.Close()
+		}()
+
+		_, err = conn.Write([]byte("SET COUNTRY IDN\n"))
+
+		if err != nil {
+			channel <- 0
+		}
+
+		reader := bufio.NewReader(conn)
+
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			channel <- 0
+		}
+
+		responseMsg := strings.TrimSpace(response)
+
+		if responseMsg == "OK"{
+			channel <- 1
+		} else {
+			channel <- 0
+		}
+	}()
+
+	go func ()  {
+		wg.Wait()
+		close(channel)
+	}()
+
+	
+	allSetPassed := true
+	for value := range channel{
+		if value == 0 {
+			allSetPassed = false
+			break
+		}
+
+		fmt.Println("CHANNEL VALUE", value)
+	}
+
+	fmt.Println("ALL CHANNEL HAVE BEEN RECEIVED")
+
+
+	assert.Equal(t, true, allSetPassed, "All Set Command Should Be OK")
+
+}
+
+func TestConcurentSetCommandOneServerActive_OneFail(t *testing.T){
+	startServer(t, "8080", "")
+
+	channel := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func ()  {
+		
+		conn, err := net.Dial("tcp", "localhost:8080")
+
+		if err != nil {
+			channel <- 0
+		}
+
+		
+		defer func ()  {
+			wg.Done()
+			conn.Close()
+		}()
+
+		_, err = conn.Write([]byte("SET NAME ARI\n"))
+
+		if err != nil {
+			channel <- 0
+		}
+
+		reader := bufio.NewReader(conn)
+
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			channel <- 0
+		}
+
+		responseMsg := strings.TrimSpace(response)
+
+		if responseMsg == "OK"{
+			channel <- 1
+		} else {
+			channel <- 0
+		}
+	}()
+
+	go func ()  {
+		conn, err := net.Dial("tcp", "localhost:8080")
+
+		if err != nil {
+			channel <- 0
+		}
+
+		
+		defer func ()  {
+			wg.Done()
+			conn.Close()
+		}()
+
+		_, err = conn.Write([]byte("ST COUNTRY IDN\n"))
+
+		if err != nil {
+			channel <- 0
+		}
+
+		reader := bufio.NewReader(conn)
+
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			channel <- 0
+		}
+
+		responseMsg := strings.TrimSpace(response)
+
+		if responseMsg == "OK"{
+			channel <- 1
+		} else {
+			channel <- 0
+		}
+	}()
+
+	go func ()  {
+		wg.Wait()
+		close(channel)
+	}()
+
+	
+	allSetPassed := true
+	for value := range channel{
+		if value == 0 {
+			allSetPassed = false
+			break
+		}
+
+		fmt.Println("CHANNEL VALUE", value)
+	}
+
+	fmt.Println("ALL CHANNEL HAVE BEEN RECEIVED")
+
+
+	assert.Equal(t, false, allSetPassed, "Return should be false because one command is fail")
+
+}
+
+func TestConcurentGetCommandOneServerActive_SuccessAll(t *testing.T){
+	startServer(t, "8080", "")
+
+	channel := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func ()  {
+		defer wg.Done()
+		conn, err := net.Dial("tcp", "localhost:8080")
+
+		if err != nil {
+			channel <- 0
+			return
+		}
+
+		_, err = conn.Write([]byte("SET NAME ARI\n"))
+
+		if err != nil {
+			channel <- 0
+			return
+		}
+
+		conn.Close()
+
+		conn, err = net.Dial("tcp", "localhost:8080")
+
+		_, err = conn.Write([]byte("GET NAME\n"))
+
+		reader := bufio.NewReader(conn)
+
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			channel <- 0
+			return
+		}
+
+		responseMsg := strings.TrimSpace(response)
+
+		if responseMsg == "ARI"{
+			channel <- 1
+		} else {
+			channel <- 0
+		}
+
+		conn.Close()
+	}()
+
+	go func ()  {
+		defer wg.Done()
+		conn, err := net.Dial("tcp", "localhost:8080")
+
+		if err != nil {
+			channel <- 0
+			return
+		}
+
+		_, err = conn.Write([]byte("SET COUNTRY IDN\n"))
+
+		if err != nil {
+			channel <- 0
+			return
+		}
+
+		conn.Close()
+
+		conn, err = net.Dial("tcp", "localhost:8080")
+
+		_, err = conn.Write([]byte("GET COUNTRY\n"))
+
+		reader := bufio.NewReader(conn)
+
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			channel <- 0
+			return
+		}
+
+		responseMsg := strings.TrimSpace(response)
+
+		if responseMsg == "IDN"{
+			channel <- 1
+		} else {
+			channel <- 0
+		}
+
+		conn.Close()
+	}()
+
+	go func ()  {
+		wg.Wait()
+		close(channel)
+	}()
+
+	
+	allGetPassed := true
+	for value := range channel{
+		if value == 0 {
+			allGetPassed = false
+			break
+		}
+
+		fmt.Println("CHANNEL VALUE", value)
+	}
+
+	fmt.Println("ALL CHANNEL HAVE BEEN RECEIVED")
+
+
+	assert.Equal(t, true, allGetPassed, "Return should be true because all get command return data")
+
+}
+
+func TestConcurentGetCommandOneServerActive_PartialSuccess(t *testing.T){
+	startServer(t, "8080", "")
+
+	channel := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func ()  {
+		defer wg.Done()
+		conn, err := net.Dial("tcp", "localhost:8080")
+
+		if err != nil {
+			channel <- 0
+			return
+		}
+
+		_, err = conn.Write([]byte("SET NAME ARI\n"))
+
+		if err != nil {
+			channel <- 0
+			return
+		}
+
+		conn.Close()
+
+		conn, err = net.Dial("tcp", "localhost:8080")
+
+		_, err = conn.Write([]byte("GET NAME\n"))
+
+		reader := bufio.NewReader(conn)
+
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			channel <- 0
+			return
+		}
+
+		responseMsg := strings.TrimSpace(response)
+
+		if responseMsg == "ARI"{
+			channel <- 1
+		} else {
+			channel <- 0
+		}
+
+		conn.Close()
+	}()
+
+	go func ()  {
+		defer wg.Done()
+		conn, err := net.Dial("tcp", "localhost:8080")
+
+		if err != nil {
+			channel <- 0
+			return
+		}
+
+		_, err = conn.Write([]byte("SET COUNTRY IDN\n"))
+
+		if err != nil {
+			channel <- 0
+			return
+		}
+
+		conn.Close()
+
+		conn, err = net.Dial("tcp", "localhost:8080")
+
+		_, err = conn.Write([]byte("GET STREET\n"))
+
+		reader := bufio.NewReader(conn)
+
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			channel <- 0
+			return
+		}
+
+		responseMsg := strings.TrimSpace(response)
+
+		if responseMsg == "NOT_FOUND"{
+			channel <- 1
+		} else {
+			channel <- 0
+		}
+
+		conn.Close()
+	}()
+
+	go func ()  {
+		wg.Wait()
+		close(channel)
+	}()
+
+	
+	allGetPassed := true
+	for value := range channel{
+		if value == 0 {
+			allGetPassed = false
+			break
+		}
+
+		fmt.Println("CHANNEL VALUE", value)
+	}
+
+	fmt.Println("ALL CHANNEL HAVE BEEN RECEIVED")
+
+
+	assert.Equal(t, true, allGetPassed, "Return should be true because all one get command return correct data and one return not found")
+
 }
 
 func startServer(t *testing.T, port string, leader string) *exec.Cmd {
